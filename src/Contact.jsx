879e595458c1e3ebb10a4b5fd86880a48365e5cf
Pentarok <makpentarok@gmail.com>
2025-02-01 20:42:57 +0300

    third commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = ({isHome}) => {
     
     try {
       setLoading(true);
-      const response = await axios.post("https://church-backend-five.vercel.app/api/contact",{name,email,message});
+      const response = await axios.post("https://church-backend-five.vercel.app/api/contact",{name,email,message},{withCredentials:true});
       setLoading(false);
       setAlertMessage(response.data.message);
   
